e7da213ddb3591eb8772f04247584f18afdcd71e
vladishum <vladimirsumanski@yahoo.com>
2023-11-25 15:08:57 +0100

    New C# Project
    New basic C# Project

---
+++ Acme Accounting Systems/Acme Accounting Systems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we're \"accounting\" on you!");
+        Console.Read();
+        }
+    }
+
