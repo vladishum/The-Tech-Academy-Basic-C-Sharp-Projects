5316db135fb1c5993087cf84809e271a4c81255a
vladishum <vladimirsumanski@yahoo.com>
2023-11-28 12:41:07 +0100

    String Assignment
    String Examples with C#

---
+++ String Assignment/String Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initializing strings
+            string fisrtString = "I am ";
+            string secondString = "Bond, ";
+            string thirdString = "James Bond";
+
+            Console.WriteLine(fisrtString + secondString + thirdString);//Concatenate strings
+            Console.WriteLine(thirdString.ToUpper());//Make the string with all upper letters
+
+            //Creating string with StringBuilder
+            StringBuilder sb = new StringBuilder();
+            sb.Append("This is a String Builder. ");
+            sb.Append("It is used for dynamic creation of strings. ");
+            sb.Append("It is good for memory usage. ");
+            sb.Append("You should try it.");
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+        }
+    }
+}
