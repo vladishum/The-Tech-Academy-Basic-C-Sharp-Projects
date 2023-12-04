45fc179dbb0826ec0426a2854146bf25e34f036d
vladishum <vladimirsumanski@yahoo.com>
2023-12-04 16:53:54 +0100

    Calling Methods Assignment
    Simple program example for calling class methods in C#

---
+++ Calling Methods Assignment/Calling Methods Assignment/MethodClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    //Class for three math methods
+    class MethodClass
+    {
+        //Method to divide user's input number by 2
+        public static int DivideByTwo(int divide)
+        {
+            int result = divide / 2;
+            return result;
+        }
+
+        //Method to multiply user's input by 10
+        public static int MultiplyByTen(int multiply)
+        {
+            int result = multiply * 10;
+            return result;
+        }
+
+        //Method to raise user's input to the power of two
+        public static int PowerOfTwo(int powerOfTwo)
+        {
+            int result = powerOfTwo * powerOfTwo;
+            return result;
+        }
+    }
+}
---
+++ Calling Methods Assignment/Calling Methods Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user which number they want and get that number
+            Console.WriteLine("What number do you want the operations on?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //Call the methods from the MethodClass and display the result
+            Console.WriteLine(number + " divided by 2 is " +MethodClass.DivideByTwo(number));
+            Console.WriteLine(number + " multiplyed by 10 is " + MethodClass.MultiplyByTen(number));
+            Console.WriteLine(number + " to the power of two is " + MethodClass.PowerOfTwo(number));
+
+            Console.ReadLine();
+        }
+    }
+}
