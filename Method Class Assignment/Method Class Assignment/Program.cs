11ab6996903855143cf77957f284cd5a4e0715b7
vladishum <vladimirsumanski@yahoo.com>
2023-12-06 14:23:35 +0100

    Method Class Assignment

---
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMethod object1 = new VoidMethod();//Instance of the VoidMethod class
+            object1.assignmentMethod(44, 55);//Call assignmentMethod() with two parametars
+            object1.assignmentMethod(number1: 44, number2: 55);//Call assignmentMethod() with two parametars specifying by name
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method Class Assignment/Method Class Assignment/VoidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class VoidMethod
+    {
+        //Void method with two parametars 
+        public void assignmentMethod(int number1, int number2)
+        {
+            int result = number1 % number2;//Do a math operation on first integerr
+            Console.WriteLine(number2);//Display the second integer
+        }
+    }
+}
