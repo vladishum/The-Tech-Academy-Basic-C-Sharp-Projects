519dac530f4fc94f1fe6e7623fc72721561956b7
vladishum <vladimirsumanski@yahoo.com>
2023-12-06 12:14:29 +0100

    Main Method Assignment
    Simple program calling functions and overloading them

---
+++ Main Method Assignment/Main Method Assignment/IntegerOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class IntegerOperations
+    {
+        //Create function FunctionInteger with integer parametar and return value integer
+        public int FunctionInteger(int number)
+        {
+            int result = number * 10;
+            return result;
+        }
+
+        //Overload function FunctionInteger with decimal parametar and return value integer
+        public int FunctionInteger(decimal number)
+        {
+            int result = Convert.ToInt32(number) / 10;
+            return result;
+        }
+
+        //Create function FunctionInteger with string parametar and return value integer
+        public int FunctionInteger(string number)
+        {
+            //Check the format of the argument
+            try
+            {
+                int result = Convert.ToInt32(number) * 10;
+                return result;
+            }
+
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }         
+        }
+    }
+}
---
+++ Main Method Assignment/Main Method Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the IntegerOperations class
+            IntegerOperations object1 = new IntegerOperations();
+
+            //Call the overloaded FunctionInteger with 3 different parametars
+            Console.WriteLine(object1.FunctionInteger(10));
+            Console.WriteLine(object1.FunctionInteger(10.35m));
+            Console.WriteLine(object1.FunctionInteger("rfre"));
+
+            Console.ReadLine();
+        }
+    }
+}
