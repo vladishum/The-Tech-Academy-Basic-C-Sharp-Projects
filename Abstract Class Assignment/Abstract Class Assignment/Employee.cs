5b845818ef4cb009c41d43bddf9f4179508d5416
vladishum <vladimirsumanski@yahoo.com>
2023-12-08 20:28:53 +0100

    Abstract Class Assignment
    Simple example of abstract class

---
+++ Abstract Class Assignment/Abstract Class Assignment/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    //Class that inherits from Person class
+    public class Employee : Person
+    {
+        //Override the method SayName
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ Abstract Class Assignment/Abstract Class Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantied class Employee and add value to the properties and call the SayName method
+            Employee Object1 = new Employee() { FirstName = "Sample", LastName = "Student"};
+            Object1.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
