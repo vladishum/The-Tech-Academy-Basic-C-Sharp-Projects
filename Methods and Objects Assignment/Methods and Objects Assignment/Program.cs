3744ee0fcc776d46bbd2c8aa54453100af641c96
vladishum <vladimirsumanski@yahoo.com>
2023-12-08 14:14:36 +0100

    Methods and Objects Assignment
    Simple example of inheritance on object oriented programming

---
+++ Methods and Objects Assignment/Methods and Objects Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create object of type Employee and initialize the properties FirstName and LastName
+            Employee Object1 = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //Call the method SayName on the new object
+            Object1.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
