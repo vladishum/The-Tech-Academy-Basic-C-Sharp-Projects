7e2f689013d3edde204d3cea854eded0152b94ef
vladishum <vladimirsumanski@yahoo.com>
2023-12-09 12:57:57 +0100

    Operators Assignment

---
+++ Operators Assignment/Operators Assignment/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Overload the Equals method for objects
+        public override bool Equals(object o)
+        {
+            if (o == null)
+                return false;
+
+            var second = o as Employee;
+
+            return second != null && Id == second.Id;
+        }
+
+        //The compiler asks for this override as well 
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+        //Overload the operator == on weather the Id property
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;          
+        }
+
+        //Overload the operator != on weather the Id property
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+    }
+}
---
+++ Operators Assignment/Operators Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate objects Employee with same Id
+            Employee employee1 = new Employee() { Id = 5, FirstName = "John", LastName = "Smith"};
+            Employee employee2 = new Employee() { Id = 5, FirstName = "Steve", LastName = "Gold"};
+
+            //Check if the objects are equal based on Id
+            if(employee1 == employee2)
+            {
+                Console.WriteLine("Employee with same Id");
+            }
+            else
+            {
+                Console.WriteLine("Employee with different Id");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
