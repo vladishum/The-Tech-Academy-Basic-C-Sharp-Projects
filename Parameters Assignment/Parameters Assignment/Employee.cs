e0ac523f1525ab7a73e4fff206afa64822805bc3
vladishum <vladimirsumanski@yahoo.com>
2023-12-09 14:24:32 +0100

    Parameters Assignment
    Generic Class Parameters Example

---
+++ Parameters Assignment/Parameters Assignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Assignment
+{
+    //Create class with generic type parameter
+    public class Employee<T>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+        public List<T> Things { get; set; } //List with the generic type parameter of the class
+    }
+}
---
+++ Parameters Assignment/Parameters Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> Employee1 = new Employee<string>(); //Object with string type as generic parameter
+            Employee1.Things = new List<string>() { "Gloves", "Shoes", "Hat", "Hammer", "Screwdriver" }; //Make the Things property list of strings
+
+            Employee<int> Employee2 = new Employee<int>(); //Object with int type as generic parameter
+            Employee2.Things = new List<int>() { 10, 20, 30, 40, 50 }; //Make the Things property list of integers
+
+            foreach (string thing in Employee1.Things)
+            {
+                Console.WriteLine(thing); //Display the property Things of the Employee1 object
+            }
+
+            foreach (int thing in Employee2.Things)
+            {
+                Console.WriteLine(thing); //Display the property Things of the Employee2 object
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
