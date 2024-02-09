58f3c66dc378e38f03166e788732415ef2df44ed
vladishum <vladimirsumanski@yahoo.com>
2024-02-09 19:25:30 +0000

    TryCatchAssignment
    Simple Example of try catch block and throwing an exception

---
+++ TryCatch Assignment/TryCatch Assignment/LessOrZero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch_Assignment
+{
+    public class LessOrZero : Exception//Exception to handle less or zero numbers
+    {
+        public LessOrZero() : base() { }
+        public LessOrZero(string message) : base(message) { }
+    }
+}
---
+++ TryCatch Assignment/TryCatch Assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int answer;
+            bool valid = false;
+            while (!valid)//Do the loop until you get the proper input
+            {
+                try//try block to check that the unput is a number and greter than zero and not negative
+                {
+                    Console.WriteLine("What's your age?");
+                    answer = Convert.ToInt32(Console.ReadLine());//Get the input
+                    if (answer > 0)//If it is greater than zero OK
+                    {
+                        Console.WriteLine("You were born " + (Convert.ToInt32(DateTime.Now.Year.ToString()) - answer));
+                        valid = true;
+                        Console.ReadLine();
+                    }
+                    else throw new LessOrZero();//Else throw exception
+                }
+                catch (LessOrZero)//Catch if number is less or zero
+                {
+                    Console.WriteLine("No negative numbers or zero! ");
+                }
+                catch (Exception)//Catch for any other exception
+                {
+                    Console.WriteLine("Enter number, no decimals please!");
+                    valid = false;
+                }
+            }
+           
+        }
+    }
+}
