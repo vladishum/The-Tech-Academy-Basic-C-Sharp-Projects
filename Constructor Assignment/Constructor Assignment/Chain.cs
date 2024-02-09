744369ae372ec60240a5f5387e6925a7aa7c777d
vladishum <vladimirsumanski@yahoo.com>
2024-02-09 10:33:12 +0000

    Constructor Assignment
    Simple Constructor chaining, var example and const example

---
+++ Constructor Assignment/Constructor Assignment/Chain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    public class Chain
+    {
+        public Chain(float num1) : this(num1, "Bob")
+        {
+            Console.WriteLine(num1 + " and your default name is " + Name);
+        }
+
+        //Constructor if one parametar , a string is provided
+        public Chain(string name1) : this(42, name1)
+        {
+            Console.WriteLine(name1 + " and your default favorite number is " + Num);
+        }
+
+        //Constructor if both parametars, a string and a float are provided
+        public Chain(float num, string name)
+        {
+            Name = name;
+            Num = num;
+        }
+
+        string Name { get; set; }
+        float Num { get; set; }
+    }
+}
