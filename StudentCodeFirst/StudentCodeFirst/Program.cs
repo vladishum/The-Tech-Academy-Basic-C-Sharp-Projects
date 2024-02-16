6a759d610a1e0d9a5a221f4086411437e612cd25
vladishum <vladimirsumanski@yahoo.com>
2024-02-16 13:08:35 +0000

    Student Code-First Assignment
    A simple example of Code-First entityframework example

---
+++ StudentCodeFirst/StudentCodeFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill",
+                    DateOfBirth = Convert.ToDateTime("28/09/1991"),
+                    Height = 180.0m,
+                    Weight = 90
+                };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
